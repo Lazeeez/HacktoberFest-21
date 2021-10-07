f863c117470d65cd19568ee68af1c9e8c7c26b52
Digvijaysing Rajput <geniusdigvijaysing@gmail.com>
2021-10-07 19:40:25 +0530

    New Python Codes

---
+++ Programs/repwordcout.py
@@ -0,0 +1,10 @@
+# Check the repeated word in string & count them
+
+import collections
+input_string=input(str())
+output=collections.Counter(input_string)
+
+values = output.values()
+values_list = list(values)
+ans=values_list.count(1)
+print(ans)
