6fd0341c237f2881a2c1689e28942a6abe9861df
Saloni Shukla <saloni.shukla@ClaimGeniusInc.com>
2021-10-07 20:46:31 +0530

    Changes Made

---
+++ Programs/Score_Card.py
@@ -0,0 +1,15 @@
+
+#Normalizing the Score card using ranges
+
+x = 3.76
+
+if (0<= x < 1):
+    print('[0-1)')
+elif (1 <= x < 2):
+    print('[1-2)')
+elif (2 <= x < 3):
+    print( '[2-3)')
+elif (3<= x < 4):
+    print('[3-4)')
+else:
+    print("Out of Bound")
