f363f5cafa8f6cf0d6f6d5c2942881b6619eab9b
Surojit <81943886+thesurojit-das@users.noreply.github.com>
2021-10-11 00:15:23 +0530

    Update steps.cpp

--- Programs/steps.cpp
+++ Programs/steps.cpp
@@ -1,22 +1,17 @@
 #include<iostream>
-
-int
-main ()
+using namespace std;
+int main ()
 {
   for (int i = 0; i < 5; ++i)
     {
-
       for (int j = i; j < 5; ++j)
 	{
-	  std::cout << " ";
-
+	 cout << " ";
 	}
       {
 	for (int k = 0; k < i; ++k)
-	  std::cout << "#";
+	  cout << "#";
       }
-
-      std::cout << std::endl;
-
+	  cout <<endl;
     }
 }
