8c2f8b621d65d0e89d6c92a37c52e181711ce169
rahmah18 <56541229+rahmah18@users.noreply.github.com>
2021-10-15 06:40:01 +0100

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -19,3 +19,14 @@ def fibonacci(n):
 # Driver Program
  
 print(fibonacci(9))
+
+def fib():
+     n = int(input())
+     a = 0
+     b = 1
+     p = []
+     for i in range(n):
+         p.append(a)
+         a,b = b,a+b
+     print(p)
+fib()
